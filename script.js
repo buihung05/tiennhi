b19cb90fa9ef6f8c84de5f949680577f904d36cc
Bui Hung <buihung792005@gmail.com>
2026-03-05 19:10:05 -0800

    update

--- script.js
+++
@@ -1,5 +0,0 @@
-const music = document.getElementById("music");
-
-document.addEventListener("click", function () {
-    music.play();
-});
---
+++ script/script.js
@@ -0,0 +1,24 @@
+// nút chuyển trang
+const btn = document.getElementById("openPage");
+
+if (btn) {
+    btn.addEventListener("click", function () {
+        window.location.href = "../page/eiu.html";
+    });
+}
+
+
+// phát nhạc khi click lần đầu
+window.onload = function () {
+
+    const music = document.getElementById("music");
+
+    if (music) {
+        document.addEventListener("click", function playMusic() {
+            music.play().catch(() => {});
+            document.removeEventListener("click", playMusic);
+        });
+    }
+
+};
+
